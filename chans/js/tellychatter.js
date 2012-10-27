8c36e3ebb851c7b84dedaa68941bd9d6b0eb53a7
Paul King <cronald@gmail.com>
2012-10-27 06:16:11 +0100

    Broken timestamps

--- chans/js/tellychatter.js
+++ chans/js/tellychatter.js
@@ -1,10 +1,11 @@
 
 $(document).ready(function() {
   var now = parseInt(new Date().getTime() / 1000, 10),
-    url = "http://uk.zeebox.com/tms/schedules.json?provider=1&region=1&now=" + now + "&when=prevnownext&min-fresh=60&hd=true&view=compact&tvc=uk";
-    proxyUrl = "proxy.php?url=" + encodeURIComponent(url);
+    //url = "http://uk.zeebox.com/tms/schedules.json?provider=1&region=1&now=" + now + "&when=prevnownext&min-fresh=60&hd=true&view=compact&tvc=uk";
+    //proxyUrl = "" + encodeURIComponent(url) + "&callback=?";
+    proxyUrl = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Fuk.zeebox.com%2Ftms%2Fschedules.json%3Fprovider%3D1%26region%3D1%26now%3D" + now + "%26when%3Dprevnownext%26min-fresh%3D60%26hd%3Dtrue%26view%3Dcompact%26tvc%3Duk%22&format=json&callback=?";
 
-  console.log(url);
+  console.log(now);
 
   $("#channels li").live("click", function(event) {
     var $r = $(this);
@@ -19,27 +20,26 @@ $(document).ready(function() {
     });
   });
 
-  $.ajax({
-    url: proxyUrl,
-    dataType: "json",
-    success: function(response) {
-      var $channels = $("#channels");
+  $.getJSON(proxyUrl, function(data) {
+    var response = data.query.results.json;
+    var $channels = $("#channels");
 
-      for( var i = 0; i < response.channels.length; i++ ) {
-        var channel = response.channels[i],
-          imgsrc = channel.service.logo.replace(/\{width\}/, "100").replace(/\{height\}/, "100"),
-          broadcast = channel.channelbroadcasts[1],
-          broadcastsrc = broadcast.image.replace(/\{width\}/, "100").replace(/\{height\}/, "100");
+    for( var i = 0; i < response.channels.length; i++ ) {
+      var channel = response.channels[i],
+        imgsrc = channel.service.logo.replace(/\{width\}/, "100").replace(/\{height\}/, "100"),
+        broadcast = channel.channelbroadcasts[1],
+        broadcastsrc = broadcast.image.replace(/\{width\}/, "100").replace(/\{height\}/, "100");
 
-        var channelImg = '<img src="' + imgsrc + '">',
-          broadcastImg = '<img src="' + broadcastsrc + '">';
+      var channelImg = '<img src="' + imgsrc + '">',
+        broadcastImg = '<img src="' + broadcastsrc + '">';
 
-        var startTime = new Date(broadcast.start_time),
-          endTime = new Date(broadcast.end_time),
-          timeStr = (startTime.getHours() < 10 ? "0" : "") + startTime.getHours() + ":" + (startTime.getMinutes() < 10 ? "0" : "") + startTime.getMinutes() + " - " + (endTime.getHours() < 10 ? "0" : "") + endTime.getHours() + ":" + (endTime.getMinutes() < 10 ? "0" : "") + endTime.getMinutes();
+      console.log(broadcast);
 
-        $channels.append('<li data-title="' + escape(broadcast.title) + '" data-chan="' + channel.logical_channel_number + '" data-id="' + broadcast.id + '" data-channelname="' + escape(channel.service.name) + '" data-channellogo="' + imgsrc + '" data-programlogo="' + broadcastsrc + '">' + channelImg + broadcastImg + '<span>' + broadcast.title + '<span class="time">' + timeStr + '</span>' + '</span></li>');
-      }
+      var startTime = new Date(broadcast.start_time),
+        endTime = new Date(broadcast.end_time),
+        timeStr = (startTime.getHours() < 10 ? "0" : "") + startTime.getHours() + ":" + (startTime.getMinutes() < 10 ? "0" : "") + startTime.getMinutes() + " - " + (endTime.getHours() < 10 ? "0" : "") + endTime.getHours() + ":" + (endTime.getMinutes() < 10 ? "0" : "") + endTime.getMinutes();
+
+      $channels.append('<li data-title="' + escape(broadcast.title) + '" data-chan="' + channel.logical_channel_number + '" data-id="' + broadcast.id + '" data-channelname="' + escape(channel.service.name) + '" data-channellogo="' + imgsrc + '" data-programlogo="' + broadcastsrc + '">' + channelImg + broadcastImg + '<span>' + broadcast.title + '<span class="time">' + timeStr + '</span>' + '</span></li>');
     }
   });
 });
